4ea60ae94bed18ff852b7d7953b5edda20dc044f
sakthiram2004 <your.email@example.com>
2025-04-06 20:05:40 +0530

    PDF-Gendrate Completed

---
+++ src/main/java/com/codelab/backend/controller/PdfController.java
@@ -0,0 +1,46 @@
+package com.codelab.backend.controller;
+
+
+import com.codelab.backend.service.PdfService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/pdf")
+public class PdfController {
+
+    private static List<byte[]> pdfFiles=new ArrayList<>();
+    private final PdfService pdfService;
+
+    public PdfController(PdfService pdfService) {
+        this.pdfService = pdfService;
+    }
+
+
+    @PostMapping("/generate")
+    public ResponseEntity<byte[]> generatePdf(@RequestBody Map<String, Object> jsonData) throws Exception {
+        byte[] pdf = pdfService.generatePdf(jsonData);
+        pdfFiles.add(pdf);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdf);
+    }
+
+    @GetMapping("/getpdf")
+    public ResponseEntity<byte[]> getPdf() throws Exception {
+
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=report.pdf")
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(pdfFiles.get(0));
+    }
+
+}
---
+++ src/main/java/com/codelab/backend/service/PdfService.java
@@ -0,0 +1,41 @@
+package com.codelab.backend.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+@Service
+public class PdfService {
+
+    private final SpringTemplateEngine templateEngine;
+
+    public PdfService(SpringTemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public byte[] generatePdf(Map<String, Object> data) throws Exception {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String html = templateEngine.process("pdf_template", context);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ITextRenderer renderer = new ITextRenderer();
+
+        renderer.getSharedContext().setPrint(true);
+        renderer.getSharedContext().setInteractive(false);
+        renderer.setDocumentFromString(html);
+
+        renderer.layout();
+        renderer.createPDF(outputStream);
+
+        return outputStream.toByteArray();
+    }
+
+
+}
